c94075945604a8d0315634927d1af62e315d5eb7
vas-ide <vas.ide2022@gmail.com>
2022-07-17 18:14:28 +0300

    Harrison continue

---
+++ Harrison/9. Strings.py
@@ -0,0 +1,25 @@
+
+
+name = 'Alex'
+age = 35
+print(f'{name} is {age} ears old')
+
+
+paragraph = "\"Python is a great language!\", said Fred. \"I don't ever remember having this much fun before.\""
+print(paragraph)
+omega_form = '\u03C9'
+
+tetta_form = '\u03f4'
+print(omega_form)
+print(tetta_form)
+
+
+item = "car"
+cost = 13499.99
+print(f'{item: <10}{cost: >10}')
+
+
+
+
+
+
