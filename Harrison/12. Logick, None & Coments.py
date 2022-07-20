351f307ea27cdada03fd08ca4dcc970e1124ddc8
vas-ide <vas.ide2022@gmail.com>
2022-07-20 12:47:51 +0300

    Harrison continue
    chapter 12 in progress

---
+++ Harrison/12. Logick, None & Coments.py
@@ -0,0 +1,18 @@
+
+
+
+age = 35
+old = age > 18
+print(old)
+name = 'Alex'
+list_symbol_full = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                    'y', 'z']
+
+list_symbol_part1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+list_symbol_part2 = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+second_half = name[0].lower() in list_symbol_part1
+print(second_half)
+
+
