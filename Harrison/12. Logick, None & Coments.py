fba8207d1f8e8de892ad103d584fbedbb5dc059d
vas-ide <vas.ide2022@gmail.com>
2022-07-20 13:05:07 +0300

    Harrison continue
    chapter 12 finished
    chapter 13 innit

--- Harrison/12. Logick, None & Coments.py
+++ Harrison/12. Logick, None & Coments.py
@@ -12,7 +12,18 @@ list_symbol_full = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 list_symbol_part1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
 list_symbol_part2 = ['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-second_half = name[0].lower() in list_symbol_part1
+second_half = name[0].lower() in list_symbol_part2
 print(second_half)
 
+names = ['Alex', 'Leo', 'Ksy', 'Tani']
+if names:
+    print('Class has enrollments')
+else:
+    print('The class is empty!')
 
+car = None
+
+if car is None:
+    print('Taxi for you!')
+else:
+    print('You have a car!')
