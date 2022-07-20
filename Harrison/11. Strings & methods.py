fade7e775d26bb5afdc3c5ff2835490c33964b26
vas-ide <vas.ide2022@gmail.com>
2022-07-20 12:10:13 +0300

    Harrison continue
    chapter 11 String & methods finished
    chapter 12 innit

--- Harrison/11. Strings & methods.py
+++ Harrison/11. Strings & methods.py
@@ -1,2 +1,13 @@
 
+a = ''
+a = '/\/'.join(['1', '2', '3'])
+print(a)
 
+
+country = 'usa'
+correct_country = country.upper()
+print(correct_country)
+filename = 'hello.py'
+print(filename.lower().endswith('.java'))
+print(filename.find('py'))
+print(filename.startswith('word'))
