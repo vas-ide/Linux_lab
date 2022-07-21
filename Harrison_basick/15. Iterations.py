6273a4489d104c304ffa6770bbe80d21237599c7
vas-ide <vas.ide2022@gmail.com>
2022-07-21 11:54:32 +0300

    Harrison
    chapter 15 in progress

---
+++ Harrison_basick/15. Iterations.py
@@ -0,0 +1,36 @@
+
+
+
+names_friends_and_colleagues = ['Alex', 'Ksy', 'Andrey', 'Viktor', 'Sergio', 'Leonid', 'Vitaliy', 'Romeo', 'Konstantin',
+                                'Leonid', 'Alexandr', 'oleg']
+
+
+len_all_names = 0
+for i in names_friends_and_colleagues:
+    len_all_names += len(i)
+print(len_all_names / len(names_friends_and_colleagues), 'Round -', round(len_all_names / len(names_friends_and_colleagues)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
