865cbfafd25d9ac712e2131473330ca84ca2df8f
vas-ide <vas.ide2022@gmail.com>
2022-07-21 10:33:06 +0300

    Harrison
    chapter 14 in progress

---
+++ Harrison_basick/14. Containers: lists, tuples & sets.py
@@ -0,0 +1,116 @@
+
+#1.________________________________________________________________
+names_list = ['Alexandr', 'Romeo', 'Sergio', 'Mihail']
+print(names_list)
+names_list_sort = sorted(names_list)
+print(names_list_sort)
+#2.________________________________________________________________
+my_data = ('Alex', 'V-----n', 35)
+print(my_data)
+people = []
+people.append(my_data)
+print(people)
+leo_data = ('Leonid', 'V-----n', 4)
+ksy_data = ('KSY', 'V-----n', 35)
+people.append(leo_data)
+people.append(ksy_data)
+sorted(people)
+print(people)
+people.sort()
+print(people)
+#3.________________________________________________________________
+friends_names = ['Alex', 'Ksy', 'Andrey', 'Viktor', 'Sergio', 'Leonid', 'Vitaliy', 'Romeo' ]
+frequent_names = ['Andrey', 'Konstantin', 'Olga', 'Any', 'Katy', 'Vitaliy', 'Sergio', 'Igor', 'Oleg', 'Alex']
+friends_names_set = set(friends_names)
+frequent_names_set = set(frequent_names)
+friends_frequent_set = frequent_names_set & friends_names_set
+print(friends_frequent_set)
+#4.________________________________________________________________
+shakespeare = '''SAMPSON.
+Gregory, on my word, we’ll not carry coals.
+
+GREGORY.
+No, for then we should be colliers.
+
+SAMPSON.
+I mean, if we be in choler, we’ll draw.
+
+GREGORY.
+Ay, while you live, draw your neck out o’ the collar.
+
+SAMPSON.
+I strike quickly, being moved.
+
+GREGORY.
+But thou art not quickly moved to strike.
+
+SAMPSON.
+A dog of the house of Montague moves me.
+
+GREGORY.
+To move is to stir; and to be valiant is to stand: therefore, if thou art moved, thou runn’st away.
+
+SAMPSON.
+A dog of that house shall move me to stand.
+I will take the wall of any man or maid of Montague’s.
+
+GREGORY.
+That shows thee a weak slave, for the weakest goes to the wall.
+
+SAMPSON.
+True, and therefore women, being the weaker vessels, are ever thrust to the wall: therefore I will push Montague’s men from the wall, and thrust his maids to the wall.
+
+GREGORY.
+The quarrel is between our masters and us their men.
+
+SAMPSON.
+’Tis all one, I will show myself a tyrant: when I have fought with the men I will be civil with the maids, I will cut off their heads.
+
+GREGORY.
+The heads of the maids?
+
+SAMPSON.
+Ay, the heads of the maids, or their maidenheads; take it in what sense thou wilt.
+
+GREGORY.
+They must take it in sense that feel it.
+
+SAMPSON.
+Me they shall feel while I am able to stand: and ’tis known I am a pretty piece of flesh.
+
+GREGORY.
+’Tis well thou art not fish; if thou hadst, thou hadst been poor John. Draw thy tool; here comes of the house of Montagues.'''
+
+ralph_waldo_emerson = '''Ralph Waldo Emerson was born in Boston, May 25, 1803. He was descended from a long line of New England ministers, men of refinement and education. As a school-boy he was quiet and retiring, reading a great deal, but not paying much attention to his lessons. He entered Harvard at the early age of fourteen, but never attained a high rank there, although he took a prize for an essay on Socrates, and was made class poet after several others had declined. Next to his reserve and the faultless propriety of his conduct, his contemporaries at college seemed most impressed by the great maturity of his mind. Emerson appears never to have been really a boy. He was always serene and thoughtful, impressing all who knew him with that spirituality which was his most distinguishing characteristic.
+
+After graduating from college he taught school for a time, and then entered the Harvard Divinity School under Dr. Channing, [6]the great Unitarian preacher. Although he was not strong enough to attend all the lectures of the divinity course, the college authorities deemed the name Emerson sufficient passport to the ministry. He was accordingly "approbated to preach" by the Middlesex Association of Ministers on October 10, 1826. As a preacher, Emerson was interesting, though not particularly original. His talent seems to have been in giving new meaning to the old truths of religion. One of his hearers has said: "In looking back on his preaching I find he has impressed truths to which I always assented in such a manner as to make them appear new, like a clearer revelation." Although his sermons were always couched in scriptural language, they were touched with the light of that genius which avoids the conventional and commonplace. In his other pastoral duties Emerson was not quite so successful. It is characteristic of his deep humanity and his dislike for all fuss and commonplace that he appeared to least advantage at a funeral. A connoisseur in such matters, an old sexton, once remarked that on such occasions "he did not appear at ease at all. To tell the truth, in my opinion, that young man was not born to be a minister."
+
+Emerson did not long remain a minister. In 1832 he preached a sermon in which he announced certain views in regard to the communion service which were disapproved by a large part of his congregation. He found it impossible to continue preaching, and, with the most friendly feelings on both sides, he parted from his congregation.
+
+A few months later (1833) he went to Europe for a short year of travel. While abroad, he visited Walter Savage Landor, Coleridge and Wordsworth, and Thomas Carlyle. This visit to Carlyle was to both men a most interesting experience. They parted feeling that they had much intellectually in common. This belief fostered a sympathy which, by the time they had discovered how different they really were, had grown so strong a habit that they always kept up their intimacy. This year of travel opened Emerson's eyes to many [7]things of which he had previously been ignorant; he had profited by detachment from the concerns of a limited community and an isolated church.
+
+After his return he began to find his true field of activity in the lecture-hall, and delivered a number of addresses in Boston and its vicinity. While thus coming before the open public on the lecture platform, he was all the time preparing the treatise which was to embody all the quintessential elements of his philosophical doctrine. This was the essay Nature, which was published in 1836. By its conception of external Nature as an incarnation of the Divine Mind it struck the fundamental principle of Emerson's religious belief. The essay had a very small circulation at first, though later it became widely known.
+
+In the winter of 1836 Emerson followed up his discourse on Nature by a course of twelve lectures on the "Philosophy of History," a considerable portion of which eventually became embodied in his essays. The next year (1837) was the year of the delivery of the Man Thinking, or the American Scholar address before the Phi Beta Kappa Society at Cambridge.
+
+This society, composed of the first twenty-five men in each class graduating from college, has annual meetings which have called forth the best efforts of many distinguished scholars and thinkers. Emerson's address was listened to with the most profound interest. It declared a sort of intellectual independence for America. Henceforth we were to be emancipated from clogging foreign influences, and a national literature was to expand under the fostering care of the Republic.
+
+These two discourses, Nature and The American Scholar, strike the keynote of Emerson's philosophical, poetical, and moral teachings. In fact he had, as every great teacher has, only a limited number of principles and theories to teach. These principles of life can all be enumerated in twenty words—self-reliance, culture, intellectual and moral independence, the divinity of nature and man, the necessity of labor, and high ideals.
+
+[8]
+Emerson spent the latter part of his life in lecturing and in literary work. His son, Dr. Edward Emerson, gave an interesting account of how these lectures were constructed. "All through his life he kept a journal. This book, he said, was his 'Savings Bank.' The thoughts thus received and garnered in his journals were indexed, and a great many of them appeared in his published works. They were religiously set down just as they came, in no order except chronological, but later they were grouped, enlarged or pruned, illustrated, worked into a lecture or discourse, and, after having in this capacity undergone repeated testing and rearranging, were finally carefully sifted and more rigidly pruned, and were printed as essays."
+
+Besides his essays and lectures Emerson left some poetry in which is embodied those thoughts which were to him too deep for prose expression. Oliver Wendell Holmes in speaking of this says: "Emerson wrote occasionally in verse from his school-days until he had reached the age which used to be known as the grand climacteric, sixty-three.... His poems are not and hardly can become popular; they are not meant to be liked by the many, but to be dearly loved and cherished by the few.... His occasional lawlessness in technical construction, his somewhat fantastic expressions, his enigmatic obscurities hardly detract from the pleasant surprise his verses so often bring with them.... The poetic license which we allow in the verse of Emerson is more than excused by the noble spirit which makes us forget its occasional blemishes, sometimes to be pleased with them as characteristic of the writer."
+
+Emerson was always a striking figure in the intellectual life of America. His discourses were above all things inspiring. Through them many were induced to strive for a higher self-culture. His influence can be discerned in all the literary movements of the time. He was the central figure of the so-called transcendental school which was so prominent fifty [9]years ago, although he always rather held aloof from any enthusiastic participation in the movement.
+
+Emerson lived a quiet life in Concord, Massachusetts. "He was a first-rate neighbor and one who always kept his fences up." He traveled extensively on his lecturing tours, even going as far as England. In English Traits he has recorded his impressions of what he saw of English life and manners.
+
+Oliver Wendell Holmes has described him in this wise: "His personal appearance was that of the typical New Englander of college-bred ancestry. Tall, spare, slender, with sloping shoulders, slightly stooping in his later years, with light hair and eyes, the scholar's complexion, the prominent, somewhat arched nose which belongs to many of the New England sub-species, thin lips, suggestive of delicacy, but having nothing like primness, still less of the rigidity which is often noticeable in the generation succeeding next to that of the men in their shirt-sleeves, he would have been noticed anywhere as one evidently a scholarly thinker astray from the alcove or the study, which were his natural habitats. His voice was very sweet, and penetrating without any loudness or mark of effort. His enunciation was beautifully clear, but he often hesitated as if waiting for the right word to present itself. His manner was very quiet, his smile was pleasant, but he did not like explosive laughter any better than Hawthorne did. None who met him can fail to recall that serene and kindly presence, in which there was mingled a certain spiritual remoteness with the most benignant human welcome to all who were privileged to enjoy his companionship."
+
+Emerson died April 27, 1882, after a few days' illness from pneumonia. Dr. Garnett in his excellent biography says: "Seldom had 'the reaper whose name is Death' gathered such illustrious harvest as between December 1880 and April 1882. In the first month of this period George Eliot passed away, in the ensuing February Carlyle followed; in April Lord Beaconsfield died, deplored by his party, nor unregretted [10]by his country; in February of the following year Longfellow was carried to the tomb; in April Rossetti was laid to rest by the sea, and the pavement of Westminster Abbey was disturbed to receive the dust of Darwin. And now Emerson lay down in death beside the painter of man and the searcher of nature, the English-Oriental statesman, the poet of the plain man and the poet of the artist, and the prophet whose name is indissolubly linked with his own. All these men passed into eternity laden with the spoils of Time, but of none of them could it be said, as of Emerson, that the most shining intellectual glory and the most potent intellectual force of a continent had departed along with him."'''
+
+
+
+
+
